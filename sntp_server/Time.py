643ce211c948c3a86e26ec9bf9d9ff7aeae426e5
FoOkySNick <eyeofstorm63@gmail.com>
2018-03-14 12:49:36 +0500

    Add files via upload

--- sntp_server/Time.py
+++ sntp_server/Time.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
 
+UTC_OFFSET = 2208988800
+
 def get_time(inaccuracy):
     """Returns time in SNTP UTC format (from UTC epoch)"""
-    time = datetime.utcnow().timestamp() + inaccuracy
+    time = datetime.now().timestamp() + UTC_OFFSET + inaccuracy
     return time
